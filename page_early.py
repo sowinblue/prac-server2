29cbe7e43a136143328e6cd1b207636f436cdb6d
sowinblue <249227634+sowinblue@users.noreply.github.com>
2026-01-20 16:59:13 +0900

    소스 오류 수정

--- page_early.py
+++ page_early.py
@@ -9,7 +9,7 @@ html_early = '''
                 <a href="/" style="display:inline-block; padding:6px 12px; background-color:#eee; border:1px solid #ccc; border-radius:4px; text-decoration:none; color:black;">처음으로 돌아가기</a>
                 <center>
                 <h1>초기</h1>
-                <img src=/stactic/early.png alt = "별나라요정 코미" style="width: 25%; border-radius: 8px;">
+                <img src=/static/early.png alt = "별나라요정 코미" style="width: 25%; border-radius: 8px;">
                 <h2>별나라 요정 코미</h2>
                 </center>
             </body>
