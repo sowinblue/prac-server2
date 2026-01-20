8b790d855d2e6f96fc0ed745786f4f19614ad20d
송우인 <sowintildeblue@gmail.com>
2026-01-20 17:05:30 +0900

    사진 추가

--- page_mid.py
+++ page_mid.py
@@ -9,6 +9,8 @@ html_mid = '''
                 <a href="/" style="display:inline-block; padding:6px 12px; background-color:#eee; border:1px solid #ccc; border-radius:4px; text-decoration:none; color:black;">처음으로 돌아가기</a>
                 <center>
                 <h1>과도기</h1>
+                <img src=/static/mid.png alt="[VOCALOID] 하츠네미쿠" style="width:20%; border-radius: 8px;">
+                <h2>[VOCALOID] 하츠네미쿠</h2>
                 </center>
             </body>
             </html>
