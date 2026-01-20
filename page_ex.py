a98ab26c6d32dc954545a83695305cd8fe3dff16
sowinblue <249227634+sowinblue@users.noreply.github.com>
2026-01-20 18:00:16 +0900

    문구 위치 조정

--- page_ex.py
+++ page_ex.py
@@ -7,8 +7,10 @@ def no_keyword(keyword):
                     <title>검색 결과 없음</title>
                 </head>
                 <body>
-                    <h1>검색 결과가 없습니다:{keyword}</h1>
                     <a href="/" style="display:inline-block; padding:6px 12px; background-color:#eee; border:1px solid #ccc; border-radius:4px; text-decoration:none; color:black;">처음으로 돌아가기</a>
+                    <center>
+                    <h1>검색 결과가 없습니다:{keyword}</h1>
+                    </center>
                 </body>
                 </html>
                 '''
