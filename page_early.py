3d990382cac034e1af3f35ef077975594e1e1a89
sowinblue <249227634+sowinblue@users.noreply.github.com>
2026-01-20 16:51:35 +0900

    사진 링크에서 png로 변경

--- page_early.py
+++ page_early.py
@@ -9,9 +9,7 @@ html_early = '''
                 <a href="/" style="display:inline-block; padding:6px 12px; background-color:#eee; border:1px solid #ccc; border-radius:4px; text-decoration:none; color:black;">처음으로 돌아가기</a>
                 <center>
                 <h1>초기</h1>
-                <img src="https://i.namu.wiki/i/kJ-NM7qjMngG4NTc_tmu1nl5I-uqDqywcgpq384kxqoWijTxaOS4rej9to3K0qbe7T9bGYFnZSa7Ph-UUeZpiKHH01GvXEOBsqAI16oZSxVCYREaCSlXsGFIl_IYwlTOmm2VT1LmsCOmOu-t1Ri80g.webp"
-                alt = "별나라요정 코미"
-                style="width: 25%; border-radius: 8px;">
+                <img src=/stactic/early.png alt = "별나라요정 코미" style="width: 25%; border-radius: 8px;">
                 <h2>별나라 요정 코미</h2>
                 </center>
             </body>
