e2ccba8431d8678e5f5dad90af335a3316400c68
sowinblue <249227634+sowinblue@users.noreply.github.com>
2026-01-20 12:57:33 +0900

    html 분리

--- main.py
+++ main.py
@@ -1,29 +1,46 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler
+from urllib.parse import urlparse, parse_qs 
+from page_main import html_main
+from page_early import html_early
+from page_mid import html_mid
+from page_late import html_late
 
 class myhandler(BaseHTTPRequestHandler):
     def do_GET(self):
+        parsed = urlparse(self.path)
+        params = parse_qs(parsed.query)
+        keyword = params.get("keyword", [""])[0]  # 입력값 없으면 빈 문자열
+
+
         self.send_response(200)
-        self.send_header('content.type', 'text/html; charset=utf-8')
+        self.send_header('Content-type', 'text/html; charset=utf-8')
         self.end_headers()
 
-        html = '''
-        <!DOCTYPE html>
-        <html lang="en">
-        <head>
-            <meta charset="UTF-8">
-            <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <title>음악 보따리</title>
-        </head>
-        <body>
-            <h1> 좋아하는 장르를 적어봐! </h1>
-        <form methon="GET">
-            <label for="genre"> 장르 </label>
-            <input type="text" id="genre placeholder="장르 입력">
-            <button>등록</button>
-        </form>
-        </body>
-        </html>
-        '''
+
+        if not keyword:
+            html = html_main
+        elif  keyword == "초기":
+            html = html_early
+        elif keyword == "과도기":
+            html = html_mid
+        elif keyword =="후기":
+            html = html_late
+            # 🔹 3) 입력이 없으면 처음 페이지 표시
+        else:
+            html = f'''
+            <!DOCTYPE html>
+            <html>
+            <head>
+                <meta charset="UTF-8">
+                <title>검색 결과 없음</title>
+            </head>
+            <body>
+                <h1>검색 결과가 없습니다: {keyword}</h1>
+                <a href="/">처음으로 돌아가기</a>
+            </body>
+            </html>
+            '''
+        
 
         self.wfile.write(html.encode('utf-8'))
 
