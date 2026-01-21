588257af7c92f3dee035844b80f7132adab27024
sowinblue <249227634+sowinblue@users.noreply.github.com>
2026-01-21 12:59:03 +0900

    출처 표기

--- page_early.py
+++ page_early.py
@@ -9,11 +9,17 @@ html_early = '''
                 <a href="/" style="display:inline-block; padding:6px 12px; background-color:#eee; border:1px solid #ccc; border-radius:4px; text-decoration:none; color:black;">처음으로 돌아가기</a>
                 <center>
                 <h1>초기</h1>
-                <img src="/static/early.png" alt = "별나라요정 코미" style="width: 25%; border-radius: 50%;">
+                <div style="width:300px; height:200px; overflow:hidden; border-radius:8px;">
+                <img 
+                    src="/static/early.png" 
+                    alt = "별나라요정 코미" 
+                    style="width:100%; height:100%; object-fit:cover;">
                 <h2>별나라 요정 코미</h2>
-                <figcaption style="font-size:12px; color:#666; text-align:center;">
+                </div>
+                    <figcaption style="font-size:12px; color:#666; text-align:center;">
                     이미지 출처: Cosmic Baton Girl Comet-san © Nippon Animation
                     </figcaption>
                 </center>
             </body>
+            </html>
             '''
--- page_mid.py
+++ page_mid.py
@@ -9,9 +9,14 @@ html_mid = '''
                 <a href="/" style="display:inline-block; padding:6px 12px; background-color:#eee; border:1px solid #ccc; border-radius:4px; text-decoration:none; color:black;">처음으로 돌아가기</a>
                 <center>
                 <h1>과도기</h1>
-                    <img src="/static/mid.png" alt="[VOCALOID] 하츠네미쿠" style="width:20%; border-radius: 50%;">
+                    <div style="width:300px; height:200px; overflow:hidden; border-radius:8px;">
+                    <img 
+                        src="/static/mid.png" 
+                        alt="[VOCALOID] 하츠네미쿠" 
+                        style="width:100%; height:100%; object-fit:cover;">
+                    </div>
                     <figcaption style="font-size:12px; color:#666; text-align:center;">
-                    이미지 출처 하츠네 미쿠 © Crypton Future Media, INC.
+                    이미지 출처 하츠네 미쿠 © Crypton Future Media, INC.py
                     </figcaption>
                 </center>
             </body>
