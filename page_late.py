9335ada14c0908ee6e62e9d8755d707fd0d6d994
sowinblue <249227634+sowinblue@users.noreply.github.com>
2026-01-21 15:33:23 +0900

    파일 위치 수정....

--- page_late.py
+++ page_late.py
@@ -7,18 +7,23 @@ html_late='''
             </head>
             <body>
                 <a href="/" style="display:inline-block; padding:6px 12px; background-color:#eee; border:1px solid #ccc; border-radius:4px; text-decoration:none; color:black;">처음으로 돌아가기</a>
-                <center>
-                <h1 style="font-size:40px;">후기</h1>
-                    <div style="width:50%; height:50%; overflow:hidden; border-radius:8px;">
+                <h1 style="font-size:30px;text-align:center;">후기</h1>
+                <h2 style="font-size:20px;text-align:center;">[SynthV] 카사네 테토</h2>
+                    <div style="
+                            width:20%;
+                            overflow:hidden;
+                            border-radius:8px;
+                            text-align:center;
+                            margin:0 auto;
+                        ">
                     <img
                         src="/static/late.png"
                         alt="카사네 테토"
-                        style="width:100%; height:100%; object-fit:cover;">
-                    </div>
+                        style="width:100%; height:100%; object-fit:cover;text-align:center;">
                     <figcaption style="font-size:12px; color:#666; text-align:center;">
                     이미지 출처: 카사네 테토 © Twindrill / AHS
                     </figcaption>
-                </center>
+                    </div>
             </body>
             </html>
             '''
