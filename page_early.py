dd7b48c7411a46c402e4c432e5943c3ba5a2e44f
송우인 <sowintildeblue@gmail.com>
2026-01-21 15:33:23 +0900

    파일 위치 수정....

--- page_early.py
+++ page_early.py
@@ -7,18 +7,23 @@ html_early = '''
             </head>
             <body>
                 <a href="/" style="display:inline-block; padding:6px 12px; background-color:#eee; border:1px solid #CCC; border-radius:4px; text-decoration:none; color:black;">처음으로 돌아가기</a>
-                <center>
-                    <h1>초기</h1>
-                    <div style="width:50%; height:50%; overflow:hidden; border-radius:8px;">
+                    <h1 style="font-size:30px; text-align:center;">초기</h1>
+                    <h2 style="font-size:20px; text-align:center;">별나라 요정 코미</h2>
+                        <div style="
+                            width:20%;
+                            overflow:hidden;
+                            border-radius:8px;
+                            text-align:center;
+                            margin:0 auto;
+                        ">
                         <img 
-                        src="/static/early.png" 
-                        alt = "별나라요정 코미" 
-                        style="width:100%; height:100%; object-fit:cover;">
-                        <h2>별나라 요정 코미</h2>
-                    <figcaption style="font-size:12px; color:#666; text-align:center;">
-                    이미지 출처: Cosmic Baton Girl Comet-san © Nippon Animation
-                    </figcaption>
-                    </div>
+                            src="/static/early.png" 
+                            alt = "별나라요정 코미" 
+                            style="width:100%; height:100%; object-fit:cover; text-align:center;">
+                        <figcaption style="font-size:12px; color:#666; text-align:center;">
+                        이미지 출처: Cosmic Baton Girl Comet-san © Nippon Animation
+                        </figcaption>
+                        </div>
                 </center>
             </body>
             </html>
